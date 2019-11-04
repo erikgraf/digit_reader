8b32fa559aec5899f06b63b25bf3ae12b174e9f4
Erik Graf <erik.graf@gmail.com>
2019-11-04 21:33:32 +0100

    Initial commit based on re-write of MNIST Digit Reader

--- app.py
+++ app.py
@@ -1,47 +1,60 @@
 # taken from https://community.canvaslms.com/thread/2595
 
-from flask import Flask, render_template,url_for, request, jsonify
+from flask import Flask, render_template, url_for, request, jsonify
 import numpy as np
-import tensorflow as tf
 from PIL import Image
 import re
 import io
 import base64
 
-import tensor as ten
-
 app = Flask(__name__)
+trainedLogReg_ = None
 
 
-@app.route('/', methods=['GET','POST'])
-def get_image(): 
+@app.route('/', methods=['GET', 'POST'])
+def get_image():
     guess = 0
-    if request.method== 'POST':
-        #requests image from url 
-        img_size = 28, 28 
-        image_url = request.values['imageBase64']  
-        image_string = re.search(r'base64,(.*)', image_url).group(1)  
-        image_bytes = io.BytesIO(base64.b64decode(image_string)) 
-        image = Image.open(image_bytes) 
-        image = image.resize(img_size, Image.LANCZOS)  
-        image = image.convert('1') 
+    if request.method == 'POST':
+        # requests image from url
+        img_size = 28, 28
+        image_url = request.values['imageBase64']
+        image_string = re.search(r'base64,(.*)', image_url).group(1)
+        image_bytes = io.BytesIO(base64.b64decode(image_string))
+        image = Image.open(image_bytes)
+        image = image.resize(img_size, Image.ANTIALIAS)
+        image = image.convert(mode="L", colors=128)
         image_array = np.asarray(image)
-        image_array = image_array.flatten()  
-        
-        with tf.Session() as sess:
-            saver = tf.train.import_meta_graph('tmp/tensor_model.meta')
-            saver.restore(sess, tf.train.latest_checkpoint('tmp/'))
+        image_array = image_array.flatten().reshape(1, -1)
+        guess = int(trainedLogReg_.predict(image_array)[0])
+        return jsonify(guess=guess)  # returns as jason format
 
-            predict_number = tf.argmax(ten.y, 1)
-            predicted_number = ten.sess.run([predict_number], feed_dict={ten.x: [image_array]})
-            guess = predicted_number[0][0]
-            guess = int(guess)
-            print(guess)
+    return render_template('index.html', guess=guess)
 
-        return jsonify(guess = guess) #returns as jason format
 
-    return render_template('index.html', guess = guess)
+def train():
+    # Download and sort the dataset
+    print("Downloading MNIST data set")
+    try:
+        from sklearn.datasets import fetch_openml
+        mnist = fetch_openml('mnist_784', version=1, cache=True)
+        mnist.target = mnist.target.astype(np.int8)  # fetch_openml() returns targets as strings
+    except ImportError:
+        from sklearn.datasets import fetch_mldata
+        mnist = fetch_mldata('MNIST original')
+    mnist["data"], mnist["target"]
+    from sklearn.utils import shuffle
+    X, y = shuffle(mnist.data, mnist.target)
+    print("MNIST data set downloaded and shuffled")
+
+    print("Starting with model training.")
+    from sklearn.linear_model import LogisticRegression
+    lr = LogisticRegression(C=100.0, random_state=1, solver='lbfgs', multi_class='ovr')
+    lr.fit(X, y)
+    print("Finished with model training.")
+    return lr
 
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    if trainedLogReg_ is None:
+        trainedLogReg_ = train()
+    app.run(debug=True, use_reloader=False)
